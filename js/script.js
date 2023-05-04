3238fbe94a3ccf211656137b4adeae9cea974533
francoromeo <francoromeo5012@gmail.com>
2023-05-04 18:32:45 -0300

    login add remove

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ document.querySelector('#menu-btn').onclick = () =>{
 document.querySelector('#close-navbar').onclick = () =>{
     navbar.classList.remove('active');
 }
+
+let registerBtn = document.querySelector('.account-form .register-btn');
+let loginBtn = document.querySelector('.account-form .login-btn');
+
+registerBtn.onclick = () =>{
+    registerBtn.classList.add('active');
+    loginBtn.classList.remove('active');
+    document.querySelector('.account-form .login-form').classList.remove('active');
+    document.querySelector('.account-form .register-form').classList.add('active');
+}
+loginBtn.onclick = () =>{
+    registerBtn.classList.remove('active');
+    loginBtn.classList.add('active');
+    document.querySelector('.account-form .login-form').classList.add('active');
+    document.querySelector('.account-form .register-form').classList.remove('active');
+}
